e703618d90ccb9f49b904debc28fef67595232eb
tkkiethotuan <97288041+tkkiethotuan@users.noreply.github.com>
2022-07-27 11:13:04 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function mobile_users() {
+    alert("We are creating the version for mobile please wait.")
+}
+
+function pc_users() {
+    alert("Welcome")
+    window.location.replace("/home/")
+}
